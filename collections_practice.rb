623860895a09c19df02cc8e1943800daa9205ad1
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-02-24 06:20:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,7 +23,7 @@ def count_elements array
 end
 
 def merge_data data_one, data_two
-    puts data_one[0].inspect
+    puts data_one.inspect
     puts data_two[0].inspect
 
     data_two[0].each_with_index.map { |item, index|
