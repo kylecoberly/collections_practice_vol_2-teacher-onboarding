9f841cd1c3bf3face097a41b0a0e0fdf94d4cb00
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-02-24 06:11:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
-# your code goes here
+def begins_with_r array
+  !array.any? {|item| item[0] != 'r'}
+end
+
+def contain_a array
+  array.select { |item| item.include?("a") }
+end
+
+def first_wa array
+  array.find { |item| item.match /^wa/}
+end
+
+def remove_non_strings array
+  array.reject { |item| !item.is_a? String}
+end
+
+def count_elements array
+  array.uniq.map { |item|
+    matches = array.select { |item_to_match| item_to_match[:name] == item[:name] }
+    item[:count] = matches.length
+    item
+  }
+end
+
+def merge_data data_one, data_two
+    puts data_two.inspect
+    puts data_one.inspect
+    data_one.each_with_index.map { |item, index|
+      data_two[index]["blake"].merge(item)
+    }
+end
