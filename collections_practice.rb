65afa1fa5a74f413e24e5172a83bb70c7688c3df
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-02-24 06:38:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,7 +39,7 @@ end
 def organize_schools schools
   puts schools.inspect
   schools.reduce({}) { |accumulator, school|
-    puts school[1].inspect
+    puts school[1][:location]
     accumulator[school[1][:location]] << school[0]
   }
 end
